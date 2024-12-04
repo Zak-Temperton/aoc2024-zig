4ba8044406e6d2ef8faa50edb83f527dcbe64f79
Zak-Temperton <zbtemperton@gmail.com>
2024-12-04 19:49:12 +1300

    tidy day04

--- src/days/day04.zig
+++ src/days/day04.zig
@@ -10,33 +10,7 @@ pub fn run(alloc: std.mem.Allocator, stdout: anytype) !void {
     const p1_time = timer.lap();
     const p2 = part2(140, buffer);
     const p2_time = timer.read();
-    try stdout.print("Day02:\n  part1: {d} {d}ns\n  part2: {d} {d}ns\n", .{ p1, p1_time, p2, p2_time });
-}
-
-fn readInt(comptime T: type, input: []const u8, i: *usize) T {
-    var num: T = 0;
-    while (i.* < input.len) : (i.* += 1) {
-        switch (input[i.*]) {
-            '0'...'9' => |c| num = num * 10 + c - '0',
-            else => return num,
-        }
-    }
-    return num;
-}
-
-fn nextLine(input: []const u8, i: *usize) void {
-    while (i.* < input.len and (input[i.*] == '\r' or input[i.*] == '\n')) : (i.* += 1) {}
-}
-
-fn readLine(comptime width: usize, input: []const u8, i: *usize) [width]u8 {
-    var line: [width]u8 = .{};
-    var idx: usize = 0;
-    while (i.* < input.len and input[i.*] != '\r' and input[i.*] != '\n') : (i.* += 1) {
-        line[idx] = input[i.*];
-        idx += 1;
-    }
-    nextLine(input, i);
-    return line;
+    try stdout.print("Day04:\n  part1: {d} {d}ns\n  part2: {d} {d}ns\n", .{ p1, p1_time, p2, p2_time });
 }
 
 fn part1(comptime width: usize, input: []const u8) u32 {
