0ff4a82c2017941d1780170d3e40a23c107c981f
Zak-Temperton <zbtemperton@gmail.com>
2024-12-16 13:10:59 +1300

    day15 part2

--- src/days/day15.zig
+++ src/days/day15.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const ONE: u64 = 1;
+const ONE: u128 = 1;
 const THREE: u128 = 0b11;
 
 pub fn run(alloc: std.mem.Allocator, stdout: anytype) !void {
@@ -16,7 +16,7 @@ pub fn run(alloc: std.mem.Allocator, stdout: anytype) !void {
     try stdout.print("Day15:\n  part1: {d} {d}ns\n  part2: {d} {d}ns\n", .{ p1, p1_time, p2, p2_time });
 }
 
-fn pushBox(dir: u8, px: *u32, py: *u32, walls: []u64, boxes: []u64) void {
+fn pushBox(dir: u8, px: *u32, py: *u32, walls: []u128, boxes: []u128) void {
     switch (dir) {
         '^' => {
             if (py.* == 0) return;
@@ -107,19 +107,18 @@ fn pushBox(dir: u8, px: *u32, py: *u32, walls: []u64, boxes: []u64) void {
 }
 
 fn part1(input: []const u8) usize {
-    const width: u32 = 48;
-    const height: u32 = 48;
-    var walls: [height]u64 = undefined;
+    const side: u32 = 48;
+    var walls: [side]u128 = undefined;
     @memset(&walls, 0);
-    var boxes: [height]u64 = undefined;
+    var boxes: [side]u128 = undefined;
     @memset(&boxes, 0);
     var player_x: u32 = 0;
     var player_y: u32 = 0;
-    for (0..height) |y| {
+    for (0..side) |y| {
         const wall = &walls[y];
         const box = &boxes[y];
-        for (0..width) |x| {
-            switch (input[(x + 1) + (y + 1) * (width + 4)]) {
+        for (0..side) |x| {
+            switch (input[(x + 1) + (y + 1) * (side + 4)]) {
                 '#' => wall.* |= ONE << @truncate(x),
                 'O' => box.* |= ONE << @truncate(x),
                 '@' => {
@@ -131,16 +130,16 @@ fn part1(input: []const u8) usize {
         }
     }
 
-    var i = (width + 4) * (height + 2) + 2;
+    var i = (side + 4) * (side + 2) + 2;
 
     while (i < input.len) : (i += 1) {
         pushBox(input[i], &player_x, &player_y, &walls, &boxes);
     }
 
     var sum: usize = 0;
-    for (0..height) |y| {
+    for (0..side) |y| {
         const box = boxes[y];
-        for (0..width) |x| {
+        for (0..side) |x| {
             if (box >> @truncate(x) & 1 == 1) {
                 sum += (y + 1) * 100 + x + 1;
             }
@@ -149,7 +148,179 @@ fn part1(input: []const u8) usize {
     return sum;
 }
 
+fn canPushCrate(dir: u8, x: u32, y: u32, walls: []u128, boxes_left: []u128, boxes_right: []u128) bool {
+    var xx: u7 = @truncate(x);
+    var yy: u7 = @truncate(y);
+    switch (dir) {
+        '^' => {
+            if (y == 0) return false;
+            yy -= 1;
+        },
+        'v' => {
+            if (y == walls.len - 1) return false;
+            yy += 1;
+        },
+        '<' => {
+            if (x == 0 or boxes_right[y] >> xx & 1 == 1 and x == 1) return false;
+            xx -= 1;
+        },
+        '>' => {
+            if (x == walls.len * 2 - 1 or boxes_left[y] >> xx & 1 == 1 and x == walls.len * 2 - 2) return false;
+            xx += 1;
+        },
+        else => return false,
+    }
+
+    if (walls[yy] >> xx & 1 == 1) return false;
+    if (dir == '^' or dir == 'v') {
+        if (boxes_left[yy] >> xx & 1 == 1) {
+            return canPushCrate(dir, xx, yy, walls, boxes_left, boxes_right) and canPushCrate(dir, xx + 1, yy, walls, boxes_left, boxes_right);
+        }
+        if (boxes_right[yy] >> xx & 1 == 1) {
+            return canPushCrate(dir, xx, yy, walls, boxes_left, boxes_right) and canPushCrate(dir, xx - 1, yy, walls, boxes_left, boxes_right);
+        }
+    } else {
+        if (boxes_left[yy] >> xx & 1 == 1) {
+            return canPushCrate(dir, xx, yy, walls, boxes_left, boxes_right);
+        }
+        if (boxes_right[yy] >> xx & 1 == 1) {
+            return canPushCrate(dir, xx, yy, walls, boxes_left, boxes_right);
+        }
+    }
+    return true;
+}
+
+fn pushCrate(dir: u8, x: u32, y: u32, walls: []u128, boxes_left: []u128, boxes_right: []u128) void {
+    var xx: u7 = @truncate(x);
+    var yy: u7 = @truncate(y);
+    switch (dir) {
+        '^' => {
+            if (y == 0) return;
+            yy -= 1;
+        },
+        'v' => {
+            if (y == walls.len - 1) return;
+            yy += 1;
+        },
+        '<' => {
+            if (x == 0) return;
+            xx -= 1;
+        },
+        '>' => {
+            if (x == walls.len * 2 - 1) return;
+            xx += 1;
+        },
+        else => return,
+    }
+
+    if (dir == '^' or dir == 'v') {
+        if (boxes_left[y] >> @truncate(x) & 1 == 1) {
+            pushCrate(dir, xx + 1, yy, walls, boxes_left, boxes_right);
+            pushCrate(dir, xx, yy, walls, boxes_left, boxes_right);
+            if (boxes_left[y] >> @truncate(x) & 1 == 1) {
+                boxes_left[yy] |= ONE << @truncate(x);
+                boxes_left[y] ^= ONE << @truncate(x);
+            }
+            if (boxes_right[y] >> @truncate(x + 1) & 1 == 1) {
+                boxes_right[yy] |= ONE << @truncate(x + 1);
+                boxes_right[y] ^= ONE << @truncate(x + 1);
+            }
+        }
+        if (boxes_right[y] >> @truncate(x) & 1 == 1) {
+            pushCrate(dir, xx, yy, walls, boxes_left, boxes_right);
+            pushCrate(dir, xx - 1, yy, walls, boxes_left, boxes_right);
+            if (boxes_left[y] >> @truncate(x - 1) & 1 == 1) {
+                boxes_left[yy] |= ONE << @truncate(x - 1);
+                boxes_left[y] ^= ONE << @truncate(x - 1);
+            }
+            if (boxes_right[y] >> @truncate(x) & 1 == 1) {
+                boxes_right[yy] |= ONE << @truncate(x);
+                boxes_right[y] ^= ONE << @truncate(x);
+            }
+        }
+    } else {
+        if (boxes_left[y] >> @truncate(x) & 1 == 1) {
+            pushCrate(dir, xx, yy, walls, boxes_left, boxes_right);
+            if (boxes_left[y] >> @truncate(x) & 1 == 1) {
+                boxes_left[y] |= ONE << @truncate(xx);
+                boxes_left[y] ^= ONE << @truncate(x);
+            }
+        } else if (boxes_right[y] >> @truncate(x) & 1 == 1) {
+            pushCrate(dir, xx, yy, walls, boxes_left, boxes_right);
+            if (boxes_right[y] >> @truncate(x) & 1 == 1) {
+                boxes_right[y] |= ONE << @truncate(xx);
+                boxes_right[y] ^= ONE << @truncate(x);
+            }
+        }
+    }
+}
+
+fn tryPushCrate(dir: u8, px: *u32, py: *u32, walls: []u128, boxes_left: []u128, boxes_right: []u128) void {
+    if (canPushCrate(dir, px.*, py.*, walls, boxes_left, boxes_right)) {
+        switch (dir) {
+            '^' => {
+                py.* -= 1;
+            },
+            'v' => {
+                py.* += 1;
+            },
+            '<' => {
+                px.* -= 1;
+            },
+            '>' => {
+                px.* += 1;
+            },
+            else => return,
+        }
+        pushCrate(dir, px.*, py.*, walls, boxes_left, boxes_right);
+        return;
+    }
+}
+
 fn part2(input: []const u8) usize {
-    _ = input; // autofix
-    return 0;
+    const side: u32 = 48;
+    var walls: [side]u128 = undefined;
+    @memset(&walls, 0);
+    var boxes_left: [side]u128 = undefined;
+    @memset(&boxes_left, 0);
+    var boxes_right: [side]u128 = undefined;
+    @memset(&boxes_right, 0);
+    var player_x: u32 = 0;
+    var player_y: u32 = 0;
+    for (0..side) |y| {
+        const wall = &walls[y];
+        const box_left = &boxes_left[y];
+        const box_right = &boxes_right[y];
+        for (0..side) |x| {
+            switch (input[(x + 1) + (y + 1) * (side + 4)]) {
+                '#' => wall.* |= THREE << @truncate(x * 2),
+                'O' => {
+                    box_left.* |= @as(u128, 1) << @truncate(x * 2);
+                    box_right.* |= @as(u128, 1) << @truncate(x * 2 + 1);
+                },
+                '@' => {
+                    player_x = @truncate(x * 2);
+                    player_y = @truncate(y);
+                },
+                else => {},
+            }
+        }
+    }
+
+    var i = (side + 4) * (side + 2) + 2;
+
+    while (i < input.len) : (i += 1) {
+        tryPushCrate(input[i], &player_x, &player_y, &walls, &boxes_left, &boxes_right);
+    }
+
+    var sum: usize = 0;
+    for (0..side) |y| {
+        const box = boxes_left[y];
+        for (0..side * 2) |x| {
+            if (box >> @truncate(x) & 1 == 1) {
+                sum += (y + 1) * 100 + x + 2;
+            }
+        }
+    }
+    return sum;
 }
